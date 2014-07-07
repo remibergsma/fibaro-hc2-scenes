fdc9c7c58002b62e010e83641f11494cbfb48532
Remi Bergsma <github@remi.nl>
2014-07-07 20:56:35 +0200

    script to update sunrise and sunset
    For use by other scripts, for example to only switch on lights when motion is detected at night.
    
    This script needs a virtual device (id=90 in example) with two buttons. One to set the sun to 'Risen' and one to 'Set'. They should update the global var 'Sun'. Add some nice icons to the buttons and you'll be able to see the status of the sun easily :-)

---
+++ updateSunriseSunset.lua
@@ -0,0 +1,42 @@
+--[[
+%% autostart
+%% properties
+%% globals
+--]]
+
+-- Script to turn off lights when no more motion is detected
+-- Remi Bergsma github@remi.nl
+-- Inspired by posts on the Fibaro forum http://forum.fibaro.com
+
+-- Note: This script needs a virtual device (id=90 in example) with two buttons. One to set the sun to 'Risen' and one to 'Set'. They should update the global var 'Sun'.
+
+-- local vars
+local sunriseHour = fibaro:getValue(1,'sunriseHour') 
+local sunsetHour = fibaro:getValue(1,'sunsetHour') 
+local BeforeSunset = 0 
+local AfterSunrise = 0
+local debug = 1
+
+if debug ==1 then fibaro:debug("Today, sunrise is at " .. sunriseHour .. " and sunset at " .. sunsetHour) end
+
+while true do 
+  if (os.date("%H:%M", os.time()+BeforeSunset*60) >= sunsetHour) 
+    or (os.date("%H:%M", os.time()-AfterSunrise*60) < sunriseHour) 
+      then 
+        -- sun is set
+        if (fibaro:getGlobalValue("Sun") ~= "Set")
+          then
+            fibaro:call(90, "pressButton", "2")
+            if debug ==1 then fibaro:debug("Changed sunset var to Set") end
+          end
+      else
+        -- sun is risen
+        if (fibaro:getGlobalValue("Sun") ~= "Risen")
+          then
+            fibaro:call(90, "pressButton", "1")
+            if debug ==1 then fibaro:debug("Changed sunset var to Risen") end
+        end
+  end
+  -- running once a minute is enough
+  fibaro:sleep(60000)
+end
