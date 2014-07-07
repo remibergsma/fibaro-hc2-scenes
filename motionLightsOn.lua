69ad295e01ed316adfd5938b95538ffb166a089d
Remi Bergsma <github@remi.nl>
2014-07-07 20:43:21 +0200

    improved script and added debug info

--- motionLightsOn.lua
+++ motionLightsOn.lua
@@ -1,27 +1,33 @@
 --[[
 %% properties
-59 value
+63 value
 %% globals
 --]]
 
--- Please note: 
--- Update the id's below to match yours.
--- This script requires global variable 'Sun' to be defined in the Variables Panel.
+-- Script to turn on lights when motion is detected
+-- Remi Bergsma github@remi.nl
 
--- id of the motion sensor. Also update trigger at the top.
-local motionSensor = 59
+-- Please note: 
+-- Update the id's below to match yours. Also update trigger at the top.
+-- This script requires global variable 'Sun' to be defined in the Variables Panel. See sunset/sunrise script for usage.
+
+-- id of the motion sensor.
+local motionSensor = 63
 -- id of the light to switch
-local light = 62
+local light = 27
 -- is the light a dimmer
 local isDimmer = 1
 -- dimmer percentage (ignored of not a dimmer)
-local dimLevel = 10 
+local dimLevel = 70
+-- whether or not to display debug messages
+local debug = 1
 
 -- check how we are invoked
 -- type=other => manual
 local startSource = fibaro:getSourceTrigger();
+if debug ==1 then fibaro:debug("We were invoked by " .. startSource["type"]) end
 
--- if motion sensor is breached
+-- if motion sensor is detected
 if (((tonumber(fibaro:getValue(motionSensor, "value")) > 0 )
       -- or we invoke manually
       or startSource["type"] == "other" )
@@ -29,11 +35,13 @@ if (((tonumber(fibaro:getValue(motionSensor, "value")) > 0 )
    and fibaro:getGlobalValue("Sun") == "Set")
 then
     -- switch on the light
-    fibaro:debug("Switched ON lights due to motion")
+    if debug ==1 then fibaro:debug("Switching ON light due to motion") end
     if isDimmer == 1
       then
+        if debug ==1 then fibaro:debug("Set dim level to " .. dimLevel .. "%") end
 	    fibaro:call(light, "setValue", dimLevel);
     else
+        if debug ==1 then fibaro:debug("Turned on the light") end
         fibaro:call(light, "turnOn");
     end
 end
